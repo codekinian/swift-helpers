c68f60e2ba9f580947ed41d7a5a2fa40a1aeeaee
Rangga Djatikusuma Lukman <rangga.djatikusuma@bigio.id>
2020-10-08 09:35:03 +0700

    Fungsi check validasi password

--- checkValidPassword.swift
+++ checkValidPassword.swift
@@ -1,3 +1,4 @@
+// Fungsi untuk melakukan validasi ke kuatan password
 static func checkValidPassword(_ password : String) -> Bool {
     let passwordTest = NSPredicate(format: "SELF MATCHES %@", "^(?=.*[a-z])(?=.*[$@$#!%*?&])[A-Za-z\\d$@$#!%*?&]{8,}")
     return passwordTest.evaluate(with: password)
